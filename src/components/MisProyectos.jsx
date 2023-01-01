f65199a251bc3f6960c49b3bc1945deaf572308f
Br1-NT <magaro.bruno@gmail.com>
2023-01-01 20:21:13 -0300

    Card Proyects style init

--- src/components/MisProyectos.jsx
+++ src/components/MisProyectos.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 import { TituloSeccion } from "./TituloSeccion";
+import proyectosData from '../proyectos.json';
+import '../css/MisProyectos.css';
+import { AiFillGithub } from 'react-icons/ai';
 export function MisProyectos() {
+
+    console.log(proyectosData);
+
     return (
-        <div>
+        <div className='container'>
             <TituloSeccion nombre='Mis Proyectos' />
+            <div className='proy-container'>
+                {
+                    proyectosData.map(proy => {
+                        return (
+                            <div className='proy-card' key={proy.nombre}>
+                                <img src={proy.img} alt="imagen ERROR" />
+                                <div className='proy-card__info'>
+                                    <h2 className='proy-card__info--nombre'>{proy.nombre}</h2>
+                                    <p>Descripcion: {proy.descripcion}</p>
+                                </div>
+                                <div>
+                                    <button>Ir al sitio <AiFillGithub /></button>
+                                </div>
+
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
         </div>
     )
 }
