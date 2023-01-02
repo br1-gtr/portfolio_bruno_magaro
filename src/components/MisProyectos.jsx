b157ba3b0b21be749a5e6d6994907bee37e7e6b1
Br1-NT <magaro.bruno@gmail.com>
2023-01-02 14:02:15 -0300

    data&style icons cards

--- src/components/MisProyectos.jsx
+++ src/components/MisProyectos.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { TituloSeccion } from "./TituloSeccion";
 import proyectosData from '../proyectos.json';
 import '../css/MisProyectos.css';
-import { AiFillGithub } from 'react-icons/ai';
+import { AiFillGithub, AiFillHtml5 } from 'react-icons/ai';
+import { DiCss3 } from 'react-icons/di';
+import { TbBrandJavascript } from 'react-icons/tb';
+import { FaBootstrap, FaReact, FaSass } from 'react-icons/fa';
 export function MisProyectos() {
 
     console.log(proyectosData);
@@ -16,9 +19,17 @@ export function MisProyectos() {
                         return (
                             <div className='proy-card' key={proy.nombre}>
                                 <img src={proy.img} alt="imagen ERROR" />
+                                <div className='proy-card__icons-tec'>
+                                    {proy.tecnologias.html ? <AiFillHtml5 size={'25'} className='icon' /> : ''}
+                                    {proy.tecnologias.css ? <DiCss3 size={'25'} className='icon' /> : 'no css'}
+                                    {proy.tecnologias.js ? <TbBrandJavascript size={'25'} className='icon' /> : ''}
+                                    {proy.tecnologias.react ? <FaReact size={'25'} className='icon' /> : ''}
+                                    {proy.tecnologias.bootstrap ? <FaBootstrap size={'25'} className='icon' /> : ''}
+                                    {proy.tecnologias.sass ? <FaSass size={'25'} className='icon' /> : ''}
+                                </div>
                                 <div className='proy-card__info'>
                                     <h2 className='proy-card__info--nombre'>{proy.nombre}</h2>
-                                    <p>Descripcion: {proy.descripcion}</p>
+                                    <p>{proy.descripcion}</p>
                                 </div>
 
                                 <div className='proy-card__btn'>
