5e804a19e525c2d89fd65501bf3ad8bb729d7e58
Br1-NT <magaro.bruno@gmail.com>
2023-01-29 21:29:13 -0300

    btn proyects

--- src/components/MisProyectos.jsx
+++ src/components/MisProyectos.jsx
@@ -6,6 +6,7 @@ import { AiFillGithub, AiFillHtml5 } from 'react-icons/ai';
 import { DiCss3 } from 'react-icons/di';
 import { TbBrandJavascript } from 'react-icons/tb';
 import { FaBootstrap, FaReact, FaSass } from 'react-icons/fa';
+import { BsArrowUpRightCircleFill } from 'react-icons/bs';
 export function MisProyectos() {
 
     console.log(proyectosData);
@@ -20,12 +21,12 @@ export function MisProyectos() {
                             <div className='proy-card' key={proy.nombre}>
                                 <img src={proy.img} alt="imagen ERROR" />
                                 <div className='proy-card__icons-tec'>
-                                    {proy.tecnologias.html ? <AiFillHtml5 size={'25'} className='icon' /> : ''}
-                                    {proy.tecnologias.css ? <DiCss3 size={'25'} className='icon' /> : 'no css'}
-                                    {proy.tecnologias.js ? <TbBrandJavascript size={'25'} className='icon' /> : ''}
-                                    {proy.tecnologias.react ? <FaReact size={'25'} className='icon' /> : ''}
-                                    {proy.tecnologias.bootstrap ? <FaBootstrap size={'25'} className='icon' /> : ''}
-                                    {proy.tecnologias.sass ? <FaSass size={'25'} className='icon' /> : ''}
+                                    {proy.tecnologias.html && <AiFillHtml5 size={'25'} className='icon' />}
+                                    {proy.tecnologias.css && <DiCss3 size={'25'} className='icon' />}
+                                    {proy.tecnologias.js && <TbBrandJavascript size={'25'} className='icon' />}
+                                    {proy.tecnologias.react && <FaReact size={'25'} className='icon' />}
+                                    {proy.tecnologias.bootstrap && <FaBootstrap size={'25'} className='icon' />}
+                                    {proy.tecnologias.sass && <FaSass size={'25'} className='icon' />}
                                 </div>
                                 <div className='proy-card__info'>
                                     <h2 className='proy-card__info--nombre'>{proy.nombre}</h2>
@@ -35,6 +36,10 @@ export function MisProyectos() {
                                 <div className='proy-card__btn'>
                                     <button>
                                         <p>Ir al sitio</p>
+                                        <BsArrowUpRightCircleFill size={"26px"} />
+                                    </button>
+                                    <button>
+                                        <p>Repositorio</p>
                                         <AiFillGithub size={"26px"} />
                                     </button>
                                 </div>
