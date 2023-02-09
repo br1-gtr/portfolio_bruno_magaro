8cd645a542cf551dcaf6ecfff23244a7e9ca9395
Br1-NT <magaro.bruno@gmail.com>
2023-02-08 22:37:04 -0300

    sobreMi ok

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { Header } from './components/Header';
 import { NavBar } from './components/NavBar';
 import { Tecnologias } from './components/Tecnologias';
 import { MisProyectos } from './components/MisProyectos';
+import { SobreMi } from './components/SobreMi';
 function App() {
   return (
     <div className="App">
       <Header />
       <NavBar />
+      <SobreMi />
 
     </div>
   );
--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { TituloSeccion } from "./TituloSeccion.jsx";
-import imgSM from '../assets/img/sobre-mi-img.jpg';
+import { RiUser5Fill } from 'react-icons/ri';
 import '../css/SobreMi.css';
-import { FaSpotify, FaGuitar } from 'react-icons/fa';
+import { FaGuitar } from 'react-icons/fa';
 import { FiCoffee } from 'react-icons/fi';
 import { SiApplemusic } from 'react-icons/si';
 import { MdLocalMovies } from 'react-icons/md';
@@ -10,9 +10,12 @@ export function SobreMi() {
     return (
         <div className='card'>
             <div className='card__data'>
-                <img src={imgSM} alt="" className='card__img' />
                 <div>
-                    <TituloSeccion nombre='Sobre Mí' />
+                    <div className='card__title-cont'>
+                        <RiUser5Fill className='card__title--icon' size={'30'} />
+                        <TituloSeccion nombre='Sobre Mí' />
+                    </div>
+
                     <p className='card__description'>
                         Hola! Mi nombre es <strong>Bruno</strong>, nací en la ciudad de Rosario(Santa Fe - Argentina) en 1991. <strong> Soy estudiante de programación y Desarrollador Front-End</strong>.<br />
                         Actualmente me encuentro trabajando en una empresa de telecomunicaciones cómo analista command center.<br /><br />
@@ -23,7 +26,7 @@ export function SobreMi() {
                             <li><FiCoffee className='card__description--icon' /> El café</li>
                             <li><MdLocalMovies className='card__description--icon' /> Las peliculas de terror</li>
                             <li >
-                                <SiApplemusic className='card__description--icon' /> La musica... Te comparto mi lista favorita para trabajar/estudiar. <a href="" className='btn-list'>Ir a  <FaSpotify className='icon-spotify' size={'25px'} /></a>
+                                <SiApplemusic className='card__description--icon' /> La musica
                             </li>
                         </ul>
                     </p>
