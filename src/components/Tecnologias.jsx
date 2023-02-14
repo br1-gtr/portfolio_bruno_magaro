70361c25b729aacdbc1532345be3f5df81c63a34
Br1-NT <magaro.bruno@gmail.com>
2023-02-14 13:31:03 -0300

    tectextok

--- src/components/Tecnologias.jsx
+++ src/components/Tecnologias.jsx
@@ -11,9 +11,9 @@ export function Tecnologias() {
                 <TituloSeccion nombre='Conocimientos' />
             </div>
             <p className='tecnologias__description'>
-                Comencé mis primeros pasos en la programación hace algunos años con <strong>Raptor</strong>, una aplicación para programar mediante diagramas de flujo. Posteriormente pase a <strong>Java</strong> e incursione en bases de datos con <strong>MySQL</strong>.<br />
-                Mi primer acercamiento al desarrollo web fue con <strong>WordPress</strong>. Después investigar un poco esta tecnología inicie con los pilares del Front-End, <strong>HTML</strong>, <strong>CSS</strong>, <strong>JavaScript</strong>.
-                De la mano de CSS conocí <strong>Sass</strong> y varios frameworks de estilado, profundizando principalmente en <strong>Bootstrap</strong>. Por otra parte, JavaScript me llevó a conocer incontables librerías y framworks cómo <strong>ReactJs</strong>.
+                Comencé mis primeros pasos en la programación hace algunos años con <strong>Raptor</strong>, una aplicación para programar mediante diagramas de flujo. Posteriormente pasé a <strong>Java</strong> e incursioné en bases de datos con <strong>MySQL</strong>.<br />
+                Mi primer acercamiento al desarrollo web fue con <strong>WordPress</strong>. Después de investigar un poco esta tecnología inicié con los pilares del Front-End, <strong>HTML</strong>, <strong>CSS</strong>, <strong>JavaScript</strong>.
+                De la mano de CSS conocí <strong>Sass</strong> y varios frameworks de estilado, profundizando principalmente en <strong>Bootstrap</strong>. Por otra parte, JavaScript me llevó a conocer incontables librerías y frameworks cómo <strong>ReactJs</strong>.
                 Me encuentro expandiendo mis conocimientos dentro del ecosistema del desarrollo Front-End, un universo de herramientas y metodologías que crece día a día.
             </p>
             <Slider />
